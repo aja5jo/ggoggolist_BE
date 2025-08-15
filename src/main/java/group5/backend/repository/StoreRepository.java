4ae1b5a7cef39fd7736332808b72e9fc5d6ccbd0
SoominHan <soominsion0717@gmail.com>
2025-08-15 21:53:43 +0900

    ãBuild success

--- src/main/java/group5/backend/repository/StoreRepository.java
+++ src/main/java/group5/backend/repository/StoreRepository.java
@@ -32,4 +32,7 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     // 관심 카테고리 여러 개로 페이지 조회 (정렬은 Pageable로)
     Page<Store> findByCategoryIn(List<Category> categories, Pageable pageable);
 
+    boolean existsByOwnerId(Long ownerId);
+
+    Optional<Store> findByOwnerId(Long ownerId);
 }
